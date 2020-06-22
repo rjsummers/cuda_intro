1b0481d0abe37faf79ba793c2889bbe9f1aae7a3
Summers, Randall J <rsummers@ou.edu>
2020-06-22 10:37:20 -0500

    Finish going through CUDA tutorial.

---
+++ README.md
@@ -0,0 +1,2 @@
+This repository contains the code I compiled with Visual Studio while going through
+[this CUDA tutorial](https://devblogs.nvidia.com/even-easier-introduction-cuda/).
---
+++ add_grid.cu
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <math.h>
+
+// CUDA Kernel function to add the elements of two arrays on the GPU
+__global__
+void add(int n, float* x, float* y)
+{
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
+    for (int i = index; i < n; i += stride)
+        y[i] = x[i] + y[i];
+}
+
+int main(void)
+{
+    int N = 1 << 20;
+    float *x, *y;
+
+    // Allocate Unified Memory – accessible from CPU or GPU
+    cudaMallocManaged(&x, N * sizeof(float));
+    cudaMallocManaged(&y, N * sizeof(float));
+
+    // initialize x and y arrays on the host
+    for (int i = 0; i < N; i++) {
+        x[i] = 1.0f;
+        y[i] = 2.0f;
+    }
+
+    int blockSize = 256;
+    int numBlocks = (N + blockSize - 1) / blockSize;
+
+    // Run kernel on 1M elements on the GPU
+    add<<<numBlocks, blockSize>>>(N, x, y);
+
+    // Wait for the GPU to finish before accessing on CPU
+    cudaDeviceSynchronize();
+
+    // Check for errors (all values should be 3.0f)
+    float maxError = 0.0f;
+    for (int i = 0; i < N; i++)
+        maxError = fmax(maxError, fabs(y[i] - 3.0f));
+    std::cout << "Max error: " << maxError << std::endl;
+
+    // Free memory
+    cudaFree(x);
+    cudaFree(y);
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
